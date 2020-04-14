4b7865d2509a4f7374dfe7d9a6ebc9e50728b222
Nikita Kozodoi <n.kozodoi@icloud.com>
2020-04-14 16:44:51 +0200

    add function to print factor levels

--- codes/functions.py
+++ codes/functions.py
@@ -408,3 +408,33 @@ def fill_na(df, to_na_cols, to_0_cols, to_true_cols, to_false_cols):
     df[to_false_cols] = df[to_false_cols].fillna(False)
        
     return df
+
+
+
+###############################
+#                             
+#      PRINT FACTOR LEVELS
+#                             
+###############################
+
+def print_factor_levels(df, top = 5):
+    '''
+    Print levels of categorical features.
+    
+    Arguments:
+    - df = pandas DF
+    - top = how many most frequent values to display
+
+    Returns
+    - tables with factor levels
+    '''
+
+    facs = [f for f in df.columns if df[f].dtype == 'object']
+    
+    for fac in facs:
+        print('--------------------------------')
+        print(fac + ': ' + str(df[fac].nunique()) + ' unique values')
+        print('--------------------------------')
+        print(df[fac].value_counts(normalize = True, dropna = False).head(top))
+        print('--------------------------------')
+        print('')
